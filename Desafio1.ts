cf82edfec1e50d999e98c917c3295705b5d16589
FabioBRito_Mablus <98613680+mablus@users.noreply.github.com>
2022-07-31 18:37:37 -0300

    Add files via upload

---
+++ Desafio1.ts
@@ -0,0 +1,8 @@
+interface employee {
+  code: number,
+  name: string
+};
+
+const employee = {} as employee;
+employee.code = 10;
+employee.name = "Jão";
---
+++ desafio2.ts
@@ -0,0 +1,28 @@
+interface pessoa {
+    nome: string,
+    idade: number,
+    profissao: string
+  }
+
+const pessoa1:pessoa = {
+    nome : "maria",
+    idade : 29,
+    profissao : "atriz"
+};
+
+const pessoa2:pessoa = {
+    nome : "roberto",
+    idade : 19,
+    profissao : "Padeiro"
+};
+
+var pessoa3:pessoa = {
+    nome :"laura",
+    idade: 32,
+    profissao :"Atriz"
+};
+
+var pessoa4:pessoa  = {
+nome : "carlos",
+idade : 19,
+profissao : "padeiro"};
